3d24a9b93f196603f8948554bff96f3982fc9096
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-03-20 21:00:45 -0300

    add authentication

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import './index.css'
+import React from "react";
+import { render } from "react-dom";
+import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-import React from 'react'
-import {render} from 'react-dom'
+import Dashboard from "./components/Dashboard";
+import LoginForm from "./components/LoginForm";
+import requireAuth from "./components/requireAuth";
 
-import App from './App'
+const client = new ApolloClient({
+  uri: "https://myinvestments-ws-apollo-stg.herokuapp.com",
+  request: async operation => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      operation.setContext({
+        headers: {
+          authorization: token
+        }
+      });
+    }
+  }
+});
 
-render(<App/>, document.querySelector('#app'))
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Route exact path="/" component={requireAuth(Dashboard)} />
+        <Route path="/login" component={LoginForm} />
+      </div>
+    </Router>
+  </ApolloProvider>
+);
+
+render(<ApolloApp />, document.querySelector("#app"));
