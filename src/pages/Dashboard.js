cb32a36bc15bf58d4260fb48a84efbb38aa5cd55
jonathanrz <jonathanrafaelz@gmail.com>
2018-04-29 20:26:49 -0300

    Create investments component (#9)

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,9 +3,14 @@ import React from 'react'
 import requireAuth from 'app/lib/requireAuth'
 
 import Layout from 'app/components/Layout'
+import Investments from 'app/components/Investments'
 
 const Dashboard = () => {
-  return <Layout title="Dashboard">You are logged in.</Layout>
+  return (
+    <Layout title="Dashboard">
+      <Investments />
+    </Layout>
+  )
 }
 
 export default requireAuth(Dashboard)
