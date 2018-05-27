24cdfb8cc03df2ff5d3dd97620b9797d94a3221b
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-05-27 19:02:49 -0300

    Add width to heather

--- src/components/Table/Header.js
+++ src/components/Table/Header.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { fromProp } from 'app/utils/theme'
+
 const Header = styled.thead``
 
-const HeaderCell = styled.th``
+const HeaderCell = styled.th`
+  width: ${fromProp('width')};
+`
 
 function HeaderComponent({ columns }) {
   return (
     <Header data-test="header">
       <tr>
         {columns.map((column, index) => (
-          <HeaderCell key={index} data-test="cell">
+          <HeaderCell key={index} width={column.width} data-test="cell">
             {column.title}
           </HeaderCell>
         ))}
