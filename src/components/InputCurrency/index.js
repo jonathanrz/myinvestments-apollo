22c05b374bdf7cf64955124c52de120438543185
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-07-28 10:30:36 -0300

    Fix number and float currency

--- src/components/InputCurrency/index.js
+++ src/components/InputCurrency/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { InputNumber } from 'antd'
 
 import { formatCurrency } from 'app/utils/format'
 import { parseCurrency } from 'app/utils/parse'
 
-function InputCurrency(props) {
-  return <InputNumber formatter={formatCurrency} parser={parseCurrency} {...props} />
+class InputCurrency extends Component {
+  render() {
+    const { props } = this
+    return <InputNumber formatter={formatCurrency} parser={parseCurrency} {...props} />
+  }
 }
 
 export default InputCurrency
