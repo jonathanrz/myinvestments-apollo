e71c5448a0f167f24bd4c1bbe47258eaed0cceb2
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-08-18 17:44:04 -0300

    Investment Card

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { render } from 'react-dom'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import blueGrey from '@material-ui/core/colors/blueGrey'
+import pink from '@material-ui/core/colors/pink'
 
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
@@ -13,6 +16,13 @@ import NewIncome from './pages/NewIncome'
 import EditIncome from './pages/EditIncome'
 import ImportIncomes from './pages/ImportIncomes'
 import InvestmentsOfMonth from './pages/InvestmentsOfMonth'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey,
+    secondary: pink
+  }
+})
 
 const client = new ApolloClient({
   uri: __MYINVESTMENTS_API__, //eslint-disable-line no-undef
@@ -29,21 +39,23 @@ const client = new ApolloClient({
 })
 
 const ApolloApp = () => (
-  <ApolloProvider client={client}>
-    <Router>
-      <div>
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/new-investment" component={NewInvestment} />
-        <Route path="/investment/:uuid/edit" component={EditInvestment} />
-        <Route path="/investment/:uuid/add-income" component={NewIncome} />
-        <Route path="/investment/:uuid/import-incomes" component={ImportIncomes} />
-        <Route path="/investment/:uuid" component={Investment} />
-        <Route path="/income/:uuid/edit" component={EditIncome} />
-        <Route path="/investmentsOfMonth" component={InvestmentsOfMonth} />
-      </div>
-    </Router>
-  </ApolloProvider>
+  <MuiThemeProvider theme={theme}>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/login" component={Login} />
+          <Route path="/new-investment" component={NewInvestment} />
+          <Route path="/investment/:uuid/edit" component={EditInvestment} />
+          <Route path="/investment/:uuid/add-income" component={NewIncome} />
+          <Route path="/investment/:uuid/import-incomes" component={ImportIncomes} />
+          <Route path="/investment/:uuid" component={Investment} />
+          <Route path="/income/:uuid/edit" component={EditIncome} />
+          <Route path="/investmentsOfMonth" component={InvestmentsOfMonth} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  </MuiThemeProvider>
 )
 
 render(<ApolloApp />, document.querySelector('#app'))
--- src/pages/Dashboard/Investments.js
+++ src/pages/Dashboard/Investments.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Query } from 'react-apollo'
 import { Redirect } from 'react-router-dom'
+import { withStyles, withTheme } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
+import { formatDate } from 'app/utils/format'
 import Loader from 'app/components/Loader'
 import Table from 'app/components/Table'
 import query from 'app/queries/Investments'
+
+const styles = {
+  card: {
+    minWidth: 275,
+    margin: 10
+  },
+  header: {
+    marginBottom: 16,
+    fontSize: 14,
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+}
 
 const columns = [
   { title: 'Nome', key: 'name', width: '20%', type: 'text' },
@@ -20,27 +38,51 @@ class Investments extends React.Component {
   }
 
   render() {
-    const { data } = this.props
+    const { data, classes, theme } = this.props
     const { redirectTo } = this.state
 
     return redirectTo ? (
       <Redirect to={{ pathname: `/investment/${redirectTo}`, state: { uuid: redirectTo } }} push />
     ) : (
-      <Table
-        data={data}
-        columns={columns}
-        onRow={record => this.setState({ redirectTo: record.uuid })}
-      />
+      <Fragment>
+        <Table
+          data={data}
+          columns={columns}
+          onRow={record => this.setState({ redirectTo: record.uuid })}
+        />
+
+        {data.map(investment => (
+          <Card className={classes.card} key={investment.uuid}>
+            <CardContent>
+              <Typography className={classes.header} color="textSecondary">
+                <span>{investment.type}</span>
+                <span>{investment.holder}</span>
+                <span>{investment.objective}</span>
+                {investment.dueDate && (
+                  <span style={{ color: theme.palette.secondary.main }}>
+                    {formatDate(investment.dueDate)}
+                  </span>
+                )}
+              </Typography>
+              <Typography variant="headline" component="h2">
+                {investment.name}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Fragment>
     )
   }
 }
+
+const StyledInvestments = withStyles(styles)(withTheme()(Investments))
 
 const InvestmentsQuery = () => (
   <Query query={query}>
     {({ loading, error, data }) => {
       if (loading) return <Loader message={'Buscando investimentos'} />
       if (error) return error.map(error => <div key={error}>{error.message}</div>)
-      return <Investments data={data.investments} />
+      return <StyledInvestments data={data.investments} />
     }}
   </Query>
 )
